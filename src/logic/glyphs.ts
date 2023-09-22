65893c700e62699b9c5a7d785afd2ea56604a4dc
Lenni009 <guentherhanslphdyt@gmail.com>
2023-09-22 21:05:03 +0200

    add region cube glyphs

--- src/logic/glyphs.ts
+++ src/logic/glyphs.ts
@@ -1,9 +1,10 @@
 import { getRandomNumber } from './randomGenerator';
 
+function dec2HexWithPad(num: number, pad: number = 0) {
+	return num.toString(16).padStart(pad, '0');
+}
+
 function getRandomRegion(x: number, y: number, z: number) {
-	function dec2HexWithPad(num: number, pad: number = 0) {
-		return num.toString(16).padStart(pad, '0');
-	}
 
 	const [xStr, zStr] = [x, z].map(num => dec2HexWithPad(num, 3));
 	const yStr = dec2HexWithPad(y, 2);
@@ -16,8 +17,6 @@ function getRandomRegion(x: number, y: number, z: number) {
 	]
 
 	const index = getRandomNumber(0, 3);	// NoSonar need a number between 0-3
-
-	console.log(index, yStr, zStr, xStr, coordTemplates[index])
 
 	return coordTemplates[index];
 }
@@ -46,4 +45,48 @@ export function getRandomGlyphs() {
 	const planet = getRandomPlanet();
 
 	return planet + siv + regionGlyphs;
+}
+
+const extractCoordinate = (glyphs: string, index: number) => glyphs.slice(index, index + (index === 4 ? 2 : 3));
+
+export function generateCubeGlyphs(glyphs: string): string[] {
+	const boundaries = {
+		x: ['801', '7FF'],
+		y: ['81', '7F'],
+		z: ['801', '7FF'],
+	}
+
+	const x = extractCoordinate(glyphs, 9);
+	const y = extractCoordinate(glyphs, 4);
+	const z = extractCoordinate(glyphs, 6);
+
+	const xDec = parseInt(x, 16);
+	const yDec = parseInt(y, 16);
+	const zDec = parseInt(z, 16);
+
+	const xIsMax = boundaries.x[1] === x;
+	const yIsMax = boundaries.y[1] === y;
+	const zIsMax = boundaries.z[1] === z;
+
+	const cubeCoords: number[][] = [];
+
+	cubeCoords.push([yDec, zDec, xIsMax ? xDec - 1 : xDec + 1]);	// x
+	cubeCoords.push([yIsMax ? yDec - 1 : yDec + 1, zDec, xDec]);	// y
+	cubeCoords.push([yDec, zIsMax ? zDec - 1 : zDec + 1, xDec]);	// z
+	cubeCoords.push([yIsMax ? yDec - 1 : yDec + 1, zDec, xIsMax ? xDec - 1 : xDec + 1]);	// y + x
+	cubeCoords.push([yDec, zIsMax ? zDec - 1 : zDec + 1, xIsMax ? xDec - 1 : xDec + 1]);	// x + z
+	cubeCoords.push([yIsMax ? yDec - 1 : yDec + 1, zIsMax ? zDec - 1 : zDec + 1, xDec]);	// y + z
+	cubeCoords.push([yIsMax ? yDec - 1 : yDec + 1, zIsMax ? zDec - 1 : zDec + 1, xIsMax ? xDec - 1 : xDec + 1]);	// y + x + z
+
+	console.log(cubeCoords)
+
+	const hexCubeCoords: string[][] = cubeCoords.map(item => convertArrayToHexGlyphs(item));
+
+	return hexCubeCoords.map(item => getRandomPlanet() + getRandomSIV() + item.join(''));
+}
+
+function convertArrayToHexGlyphs(coordArray: number[]): string[] {
+	const hexCoords = coordArray.map(item => dec2HexWithPad(item, 3));
+	hexCoords[0] = hexCoords[0].slice(1);
+	return hexCoords;
 }
